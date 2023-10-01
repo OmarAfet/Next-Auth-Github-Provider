6afac89aba915f5464b37fba24ad803d23588996
OmarAfet <OmarAlenezy2004@gmail.com>
2023-10-01 16:46:51 +0300

    Add github repo btn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 
 function AuthButton() {
 	const { data: session } = useSession();
@@ -13,7 +14,12 @@ function AuthButton() {
 						<Image width={48} height={48} alt="User Logo" src={session.user?.image || ""} />
 						<div>{session.user?.name}</div>
 					</div>
-					<button onClick={() => signOut()}>Sign Out</button>
+					<div className="flex gap-2">
+						<button onClick={() => signOut()}>Sign Out</button>
+						<Link className="github-btn" href={"https://github.com/OmarAfet/Next-Auth-Github-Provider"}>
+							Github Repo
+						</Link>
+					</div>
 				</div>
 			</>
 		);
@@ -21,8 +27,11 @@ function AuthButton() {
 
 	return (
 		<>
-			<div className="flex justify-center px-32 py-4 items-center">
+			<div className="flex justify-center px-32 py-4 items-center gap-2">
 				<button onClick={() => signIn()}>Sign In</button>
+				<Link className="github-btn" href={"https://github.com/OmarAfet/Next-Auth-Github-Provider"}>
+					Github Repo
+				</Link>
 			</div>
 		</>
 	);
