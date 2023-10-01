acafdd44dd2aba85059ee8842ab949f63cae6176
OmarAfet <OmarAlenezy2004@gmail.com>
2023-10-01 15:38:51 +0300

    Fix Error in Issue #8766 in next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const handler = NextAuth({
+const handler = NextAuth({
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID ?? "",
